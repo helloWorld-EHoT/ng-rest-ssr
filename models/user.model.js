c798207036609753275eddc492834eb3706b493b
EHoT <a.hetmanenko@benamix.solutions>
2018-07-04 22:34:27 +0300

    installed mongoDB and mongoose.

---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+
+  name: {
+    type: String,
+    default: 'Guest'
+  },
+  login: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password : {
+    type: String,
+    required: true
+  }
+
+});
+
+mongoose.model('user', UserSchema);
--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 require('zone.js/dist/zone-node');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const ngUniversal = require('@nguniversal/express-engine');
 
 const bootstrap = require('./dist-server/main.bundle');
 
 const app = express();
+
+
+const dbName = 'rest';
+mongoose.connect(`mongodb://localhost:27017/${dbName}`, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.log(err);
+});
+
+db.once('open', () => {
+  console.log(`MongoDB connected to "${dbName}" database`);
+});
+
+  // .then(() => {console.log(`MongoDB connected to "${dbName}" database`);})
+  // .catch(err => {console.log(err);});
+
+require('./models/user.model');
+
+const User = mongoose.model('user');
+
+User.find({}).then(user => {console.log(user);}).catch(err => {console.log(err);});
+// const user = new User({
+//   name: 'Second User',
+//   login: 'user2',
+//   email: 'user2@mail.com',
+//   password : 'qwerty'
+// });
+
+// user.save().then(user => {console.log(user);}).catch(err => {console.log(err);});
 
 app.get('/', (req, res) => res.render('index', {req, res}));
 
